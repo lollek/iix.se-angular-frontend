af389fb4939e4dc6e624a4c6ccf774fa61052d86
Olle Kvarnström <olle+git@iix.se>
2016-07-10 21:09:24 +0200

    Tiny fixes to rw

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.destroy
 
-    redirect_to books_path
+    redirect_to action: :index
   end
 
   private
